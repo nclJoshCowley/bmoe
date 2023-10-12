89f41b0cd36ae1a66f3d5ae0bc46c9446614e09a
Josh Cowley <j.cowley1@ncl.ac.uk>
2023-10-12 13:32:58 +0100

    Performance improvement to `component_allocation_visualisation`

--- R/component-plots.R
+++ R/component-plots.R
@@ -6,32 +6,39 @@
 #' * each tile is coloured according to component membership.
 #'
 #' @inheritParams bmoe-package
-#' @param chain integer. Specified chain, multiple changes forces list output.
+#' @param chain integer. Specified chain(s), multiple chains forces list output.
 #'
 #' @export
 component_allocation_visualisation <- function(object, chain) {
-  invalid_chains <- setdiff(chain, seq_len(dim(object$output$z)["chain"]))
-
-  if (length(invalid_chains)) {
-    stop("Invalid chain arguments: ", toString(invalid_chains))
-  }
-
-  if (length(chain) > 1) {
-    return(lapply(chain, component_allocation_visualisation, object = object))
-  }
-
   k_labels <- get_names_from_bmoe_fit(object)$k
 
-  z_draws <-
-    tidy(object$output$z, .dimnames = "i") |>
-    dplyr::mutate(.value = factor(.data$.value, labels = k_labels))
+  if (length(chain) == 1) {
+    return(impl_c_alloc_vis(drop(object$output$z[, chain, ]), k_labels))
+  }
 
-  mapping <- ggplot2::aes(x = .data$.iter, y = .data$i, fill = .data$.value)
+  lapply(asplit(object$output$z, 2), impl_c_alloc_vis, labels = k_labels)
+}
 
-  z_draws |>
-    dplyr::filter(.data$.chain %in% chain) |>
+
+#' Implementation of `component_allocation_visualisation`
+#'
+#' Create `ggplot2` object on a chain-by-chain basis to help with `gc()`.
+#'
+#' @param x integer-valued matrix. Rows (columns) denote iterations (indices).
+#' @param labels character. Labels to use in plot and `$data`.
+#'
+#' @keywords internal
+impl_c_alloc_vis <- function(x, labels) {
+  plot_data <-
+    expand.grid(.iter = seq_len(nrow(x)), i = seq_len(ncol(x)))
+
+  plot_data$.value <- structure(c(x), levels = labels, class = "factor")
+
+  plot_data |>
     ggplot2::ggplot() +
-    ggplot2::geom_raster(mapping = mapping) +
+    ggplot2::geom_raster(ggplot2::aes(
+      x = .data$.iter, y = .data$i, fill = .data$.value
+    )) +
     ggplot2::labs(y = "Index", x = "Iterations", fill = NULL) +
     ggplot2::scale_fill_discrete(name = NULL, drop = FALSE) +
     scale_x_iterations()
