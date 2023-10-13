dc3c56b63059dfa21c6bfd00992aa7b0e276673b
Josh Cowley <j.cowley1@ncl.ac.uk>
2023-10-13 12:50:25 +0100

    Delete helper-development.R

--- tests/testthat/helper-development.R
+++
@@ -1,9 +0,0 @@
-object <-
-  bmoe(
-    example_simulate_bmoe(n_s = 20, n_loo = 5, multiple_y = TRUE, q_cens = 0.1),
-    prior = bmoe_prior(k = 3),
-    jags_n = list(n.update = 0, n.adapt = 200, n.iter = 10000)
-  )
-
-# Prediction?
-# Ordered rank-allocation plot?
