795f1b0a9cc4502be3a16c5437ba4cb8cee5a0f1
Josh Cowley <j.cowley1@ncl.ac.uk>
2025-06-29 13:34:14 +0100

    Removed workaround for knitr progress bars

--- R/bmoe-yx.R
+++ R/bmoe-yx.R
@@ -12,7 +12,7 @@ NULL
 
 #' @rdname bmoe_yx
 #' @keywords internal
-bmoe_yx <- function(y_list, x_regr, x_wt, prior, jags_n, inits) {
+bmoe_yx <- function(y_list, x_regr, x_wt, prior, jags_n, inits = NULL) {
   stopifnot(setequal(
     names(prior),
     c("k", "regr_prec", "wt_prec", "prec_shape", "prec_rate")
@@ -43,16 +43,44 @@ bmoe_yx <- function(y_list, x_regr, x_wt, prior, jags_n, inits) {
       p_wt = NCOL(x_wt)
     )
 
-  out <-
-    complete_jags_fit(
+  message("\n", "Adaptation stage ...")
+  jags_model <-
+    rjags::jags.model(
       file = jags_file,
       data = c(jags_data, prior),
       inits = inits,
-      jags_n = jags_n,
-      varnames = c("regr", "wt", "z", "prec")
+      n.adapt = jags_n$n.adapt,
+      n.chains = jags_n$n.chains
     )
 
-  return(structure(out, class = "bmoe_fit"))
+  initial_values <- jags_model$state()
+
+  if (jags_n$n.update > 0) {
+    message("\n", "Update stage ...")
+    stats::update(jags_model, n.iter = jags_n$n.update)
+  }
+
+  message("\n", "Sampling stage ...")
+
+  output <- rjags::jags.samples(
+    model = jags_model,
+    variable.names = c("regr", "wt", "z", "prec"),
+    n.iter = jags_n$n.iter,
+    thin = jags_n$n.thin
+  )
+
+  output <- lapply(output, bmoe_array)
+
+  structure(
+    list(
+      data = c(jags_data, prior),
+      model = jags_model,
+      output = output,
+      jags_n = jags_n,
+      inits = initial_values
+    ),
+    class = "bmoe_fit"
+  )
 }
 
 
